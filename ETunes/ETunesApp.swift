bce9d64520a0ef02ea230b795c2a5100b114cffe
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-12-17 19:30:40 -0600

    first commit

---
+++ ETunes/ETunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ETunesApp.swift
+//  ETunes
+//
+//  Created by Erick Spinelli Pimentel on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ETunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
